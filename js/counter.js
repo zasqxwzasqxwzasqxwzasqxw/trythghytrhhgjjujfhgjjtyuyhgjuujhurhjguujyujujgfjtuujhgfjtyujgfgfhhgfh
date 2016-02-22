613b38a5fa45a38f5488be3f9109b7bae8e42867
zasqxwzasqxwzasqxwzasqxw <francheteau.bernard@gmail.com>
2016-02-22 14:45:41 +0100

    Added files via upload

---
+++ js/counter.js
@@ -0,0 +1,46 @@
+TRUMP.counter =(function(){
+  var countNumber = 0;
+
+  function requestSend(){
+    $.ajax({
+      url: "js/counter.json"
+    })
+      .done(function(data) {
+        requestCallback(data);
+      });
+  }
+
+  function requestCallback(data) {
+    if ( console && console.log ) {
+      console.log( data);
+    }
+    countNumber = data.counter;
+    if(data){
+        setNumber();
+        updateNumberInterval();
+    }
+  }
+
+  function setNumber(){
+    $('.counter-numbers').html(countNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+  }
+
+  function updateNumberClick(){
+    countNumber++;
+    setNumber();
+  }
+
+  function updateNumberInterval() {
+    setInterval(function(){
+      countNumber += Math.floor(Math.random()*694*2)+1;
+      setNumber();
+    }, 5000)
+  }
+
+  requestSend();
+
+  return{
+    uNC: updateNumberClick
+  }
+
+})();
